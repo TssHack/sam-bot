3a4fd0737f02a84e9b596629938879533d931a5d
Ehsan <144959137+TssHack@users.noreply.github.com>
2023-11-29 20:02:06 +0330

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# ChatGPT Telegram
+Free ChatGPT Client for telegram with nodejs
+
+## Installation
+
+To install and run this project, follow these steps:
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/ReactMVC/ChatGPT-Telegram.git
+```
+
+2. Navigate into the project directory:
+
+```bash
+cd ChatGPT-Telegram
+```
+
+3. Install the dependencies:
+
+```bash
+npm install
+```
+
+4. Start bot
+
+```bash
+npm start
+```
+or
+```bash
+node index.js
+```
+
+## Developer Contact
+
+For any queries or suggestions, you can reach out to the developer:
+
+- Name: ehsan
+- Email: ehsanfazlinejad@gmail.com
+- Telegram: [@devehsan](https://t.me/devehsan)
+
+## License
+
+This project is licensed under the MIT License.
---
+++ index.js
@@ -0,0 +1,101 @@
+const { Telegraf } = require('telegraf');
+const axios = require('axios');
+
+/**
+ * Developer: ehsan fazli
+ * Github: https://github.com/TssHack
+ * Email: ehsanfazlinejad@gmail.com
+ * Telegram: @devehsan
+**/
+
+const token = 'BOT_TOKEN'; // your telegram bot token
+
+const bot = new Telegraf(token);
+
+bot.start(async (ctx) => {
+    const chatId = ctx.chat.id;
+    const msg_id = ctx.message.message_id;
+    await ctx.reply('سلام من سام عالی هستم یک دلقک برای شاد کردن اوقات شما\nسازندم : @devehsan', {
+        reply_to_message_id: msg_id
+    });
+});
+
+bot.on('text', async (ctx) => {
+    if (ctx.chat.type === 'private') {
+        const chatId = ctx.chat.id;
+        const userText = ctx.message.text;
+        const msg_id = ctx.message.message_id;
+
+        const please = await ctx.reply('لطفا کمی صبر کنید...', {
+            reply_to_message_id: msg_id
+        });
+
+        try {
+            const response = await axios.post('https://sam-alie.onrender.com', {
+                data: {
+                    message: userText
+                },
+            }, {
+                headers: {
+                    Host: "sam-alie.onrender.com",
+                    Connection: "keep-alive",
+                    Accept: "*/*",
+                    "User-Agent":
+                        "com.tappz.aichat/1.2.2 iPhone/16.3.1 hw/iPhone12_5",
+                    "Accept-Language": "en",
+                    "Content-Type": "application/json; charset=UTF-8",
+                }
+            });
+
+            if (response.status === 200) {
+                const replyText = response.data.result.choices[0].text;
+                await ctx.telegram.editMessageText(chatId, please.message_id, null, replyText, {
+                    parse_mode: 'Markdown'
+                });
+            } else {
+                throw new Error('متاسفانه خطایی رخ داده است.');
+            }
+        } catch (error) {
+            await ctx.telegram.editMessageText(chatId, please.message_id, null, 'متاسفانه خطایی رخ داده است.');
+        }
+    } else if (ctx.chat.type !== 'private' && ctx.message.text.startsWith('gpt:') || ctx.message.text.startsWith('.gpt')) {
+        const chatId = ctx.chat.id;
+        const userText = ctx.message.text.replace(/^gpt:|^\.gpt/, '').trim();
+        const msg_id = ctx.message.message_id;
+
+        const please = await ctx.reply('لطفا کمی صبر کنید...', {
+            reply_to_message_id: msg_id
+        });
+
+        try {
+            const response = await axios.post('https://sam-alie.onrender.com', {
+                data: {
+                    message: userText
+                },
+            }, {
+                headers: {
+                    Host: "sam-alie.onrender.com",
+                    Connection: "keep-alive",
+                    Accept: "*/*",
+                    "User-Agent":
+                        "com.tappz.aichat/1.2.2 iPhone/16.3.1 hw/iPhone12_5",
+                    "Accept-Language": "en",
+                    "Content-Type": "application/json; charset=UTF-8",
+                }
+            });
+
+            if (response.status === 200) {
+                const replyText = response.data.result.choices[0].text;
+                await ctx.telegram.editMessageText(chatId, please.message_id, null, replyText, {
+                    parse_mode: 'Markdown'
+                });
+            } else {
+                throw new Error('متاسفانه خطایی رخ داده است.');
+            }
+        } catch (error) {
+            await ctx.telegram.editMessageText(chatId, please.message_id, null, 'متاسفانه خطایی رخ داده است.');
+        }
+    }
+});
+
+bot.launch();
